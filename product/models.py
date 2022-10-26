cd65c124390e31787f79bef6c204f825259737a7
sakthivellabglo <sakthivel.labglo@gmail.com>
2022-10-26 17:26:34 +0530

    Html change

--- product/models.py
+++ product/models.py
@@ -2,40 +2,43 @@ from django.db import models
 from django.contrib.auth.models import User
 
 Order_choices = [
-        ('pending', 'pending'),
-        ('sucess', 'sucess'),
-        ('failed', 'failed'),
+    ('pending', 'pending'),
+    ('sucess', 'sucess'),
+    ('failed', 'failed'),
 ]
 
 class Product(models.Model):
-	title = models.CharField(max_length = 40)
-	image = models.ImageField(upload_to='images/')
-	price = models.IntegerField()
-	brand = models.CharField(max_length = 40)
-	stock = models.BooleanField()
-	def __str__(self):
-		return " {} {} {} {} {} ".format(self.title,self.image,self.price,self.brand,self.stock)
-# Create your models here.
-class Cart(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	product = models.ForeignKey(Product, on_delete=models.CASCADE)
-	quantity = models.IntegerField(default=0)
-	price = models.FloatField(blank=True)
-	date = models.DateField(auto_now_add = True)
-	is_active = models.BooleanField(default=True)
-	def __str__(self):
-		return " {} {} {} {} {} ".format(self.user,self.product,self.quantity,self.price,self.date)
+    title = models.CharField(max_length=40)
+    image = models.ImageField(upload_to='images/')
+    price = models.IntegerField()
+    brand = models.CharField(max_length=40)
+    stock = models.BooleanField()
 
-class Order(models.Model) :
+    def __str__(self):
+        return " {} {} {} {} {} ".format(self.title, self.image, self.price, self.brand, self.stock)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.FloatField(blank=True)
+    date = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return " {} {} {} {} {} ".format(self.user, self.product, self.quantity, self.price, self.date)
+
+
+class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     items = models.ManyToManyField(Cart)
     tax = models.FloatField(null=True)
     total_product_cost = models.PositiveIntegerField(null=True)
-    order_status =  models.CharField(
+    order_status = models.CharField(
         max_length=15,
         choices=Order_choices,
-        default= 'pending'
-    
-	)
+        default='pending'
+    )
+
     def __str__(self):
         return self.user.username
--- product/views.py
+++ product/views.py
@@ -6,105 +6,118 @@ from django .contrib import messages
 from product.models import Cart, Order, Product
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
-from django.db.models import Sum , F
+from django.db.models import Sum, F
+
 
 class Allproducts(ListView):
-     model = Product
-     template_name = "allproducts.html"
+    model = Product
+    template_name = "allproducts.html"
+
 
 class Searchresult(ListView):
     model = Product
     template_name = "product.html"
 
-    def get_queryset(self): 
-        query1 =self.request.GET.get("product_name")
-        object_list =Product.objects.filter(
-            (Q(title__icontains=query1) | Q(brand__icontains=query1)) & ~Q(stock__contains= "0"))
-        if object_list.exists() :
+    def get_queryset(self):
+        query1 = self.request.GET.get("product_name")
+        object_list = Product.objects.filter(
+            (Q(title__icontains=query1) | Q(brand__icontains=query1)) & ~Q(stock__contains="0"))
+        if object_list.exists():
             pass
         else:
-           messages.error(self.request, 'Products Are Not Found')
+            messages.error(self.request, 'Products Are Not Found')
         return object_list
 
+
 @login_required
-def Add_to_add(request,id):
+def Add_to_add(request, id):
     product = Product.objects.get(id=id)
-    cart,create = Cart.objects.get_or_create(
-         product= product,
-        user = request.user,
-        price = product.price )
-    a = Cart.objects.filter(product = product)
-    if a.exists():
-        a.update(quantity =cart.quantity+1)
+    cart, create = Cart.objects.get_or_create(
+        product=product,
+        user=request.user,
+        price=product.price,
+        is_active=True)
     return redirect("all")
+
 
 @login_required
 def cart_remove(request, id):
-    remove_item= Cart.objects.get(id=id)
+    remove_item = Cart.objects.get(id=id)
     remove_item.delete()
     return redirect("cart")
 
 @login_required
-def add_quntity(request,id):
+def order_remove(request, id):
+    remove_item = Cart.objects.get(id=id)
+    remove_item.delete()
+    return redirect("order")
+
+@login_required
+def add_quntity(request, id):
     product = Product.objects.get(id=id)
-    cart = Cart.objects.get(product =  product)
+    cart = Cart.objects.filter(product=product)
     qunt = request.GET.get('quantity')
-    cart.quantity = qunt
-    cart.price =  product.price 
-    cart.save()
+    cart.update(quantity=qunt)
     return redirect("cart")
+
 
 @login_required
 def add_order(request):
-    cart = Cart.objects.filter(user =request.user)
-    total =Cart.objects.filter(Q(user =request.user)).aggregate(price__sum = Sum (F('price')* F('quantity')))['price__sum']
-    cart.update(is_active = False)
-    orders = Order.objects.get_or_create(user_id = request.user.id )
-    order = Order.objects.get(user_id = request.user.id,)
+    cart = Cart.objects.filter(user=request.user)
+    total = Cart.objects.filter(Q(user=request.user)).aggregate(
+        price__sum=Sum(F('price') * F('quantity')))['price__sum']
+    cart.update(is_active=False)
+    order, orders = Order.objects.get_or_create(user_id=request.user.id)
     order.total_product_cost = int(total)
-    order.tax=18
+    order.tax = 18
     order.save()
     order.items.add(*cart)
     return redirect("order")
 
 
 class Orderproducts(ListView):
-     model = Order
-     template_name = "order.html"
-     
-     def get_queryset(self): 
-        object_list =Order.objects.filter(user = self.request.user) 
-        if object_list.exists() :
+    model = Order
+    template_name = "order.html"
+
+    def get_queryset(self):
+        object_list = Order.objects.filter(user=self.request.user)
+        if object_list.exists():
             pass
         else:
-           messages.error(self.request, ' nothing to order  ')
+            messages.error(self.request, ' nothing to order  ')
         return object_list
-    
-     def get_context_data(self, *args, **kwargs):
+
+    def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context['sub_total'] = self.get_queryset().aggregate(total_product_cost__sum = Sum(('total_product_cost') ))['total_product_cost__sum']
-        context['tax'] = self.get_queryset().aggregate(total = Sum(F('total_product_cost')* F('tax')/100))['total'] 
+        context['sub_total'] = Cart.objects.filter(user=self.request.user).aggregate(
+            total_product_cost__sum=Sum(('price')))['total_product_cost__sum']
+        context['tax'] =Cart.objects.filter(user=self.request.user).aggregate(
+            total=Sum(F('price') *18/100))['total']
         if context['sub_total'] is not None:
             context['total_price'] = context['sub_total'] + context['tax']
         return context
- 
+
+
 class ListCartItem(ListView):
     model = Cart
     context_object_name = 'cartitems'
-    template_name='list_cartitems.html'
+    template_name = 'list_cartitems.html'
 
-    def get_queryset(self): 
-        object_list =Cart.objects.filter( (Q(user = self.request.user)& Q(is_active = True)))
-        if object_list.exists() :
+    def get_queryset(self):
+        object_list = Cart.objects.filter(
+            (Q(user=self.request.user) & Q(is_active=True)))
+        if object_list.exists():
             pass
         else:
-           messages.error(self.request, ' your cart is empty  ')
+            messages.error(self.request, ' your cart is empty  ')
         return object_list
-    
+
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context['total_price'] = self.get_queryset().aggregate(Sum('price'))['price__sum'] 
+        context['total_price'] = self.get_queryset().aggregate(Sum('price'))[
+            'price__sum']
         return context
+
 
 @login_required
 def order_place(request):
