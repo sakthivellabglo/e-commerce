3cd7186d1399feade12ab38608065a4ea8f74f9d
sakthivellabglo <sakthivel.labglo@gmail.com>
2022-11-03 16:11:08 +0530

    fix total price bugs

--- product/admin.py
+++ product/admin.py
@@ -4,7 +4,11 @@ from product.models import Brand, Cart, Product, Order, Wishlist
 
 class display_product(admin.ModelAdmin):
     list_display = ('title', 'image', 'price', 'brand', 'stock')
-    search_fields = ['brand__name']
+    search_fields = ['brand__name','title']
+    list_display_links = ('title','brand')
+    list_editable = ('price','stock')
+    sortable_field_name = "title"
+    list_filter =  ('title','brand')
 
 
 class display_Cart(admin.ModelAdmin):
